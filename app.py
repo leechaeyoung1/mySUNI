834ae96634f4f384e68e1904b38221d819a2dd7e
leechaeyoung1 <167440037+leechaeyoung1@users.noreply.github.com>
2025-08-04 17:43:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -456,4 +456,5 @@ def upload():
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
